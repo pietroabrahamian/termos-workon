0fb5819f1fc983e447d40a567267134fa664947d
Pietro Ferreira Gomes Abrahamian <pietroabrahamian2018@gmail.com>
2025-04-02 17:54:21 -0300

    termiando a pagina para imprimir

--- js/ti-entrega-imprimir.js
+++ js/ti-entrega-imprimir.js
@@ -1,3 +1,72 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+// Obtendo os dados do localStorage (com fallback para evitar valores null)
+let nome = localStorage.getItem("nomeColaborador") || "Nome não encontrado";
+let email = localStorage.getItem("emailColaborador") || "E-mail não informado";
+let rg = localStorage.getItem("rg") || "Não informado";
+let cpf = localStorage.getItem("cpf") || "Não informado";
+let funcao = localStorage.getItem("funcao") || "Não informado";
+let setor = localStorage.getItem("setor") || "Não informado";
+let escolhaEmpresa = localStorage.getItem("escolhaEmpresa") || "Empresa não informada";
+let nomeMaquina = localStorage.getItem("nomeMaquina") || "Não informado";
+let patrimonio = localStorage.getItem("patrimonio") || "Não informado";
+let locadora = localStorage.getItem("locadora") || "Não informado";
+let marca = localStorage.getItem("marca") || "Não informado";
+let modelo = localStorage.getItem("modelo") || "Não informado";
+let carregador = localStorage.getItem("carregador") || "Não informado";
+let mochila = localStorage.getItem("mochila") || "Não informado";
+
+    document.title = `${nome} - Termo de Entrega`
+    divMain = document.getElementById("main").innerHTML = ` <p>Eu, <span>${nome}</span>, portador do RG nº <span>${rg}</span>, CPF:  <span>${cpf}</span> funcionário da empresa: <span>${escolhaEmpresa}</span>,  no exercício da função de    <span> ${funcao}</span> do setor <span> ${setor}</span>, declaro que recebi nesta data, user ID de rede corporativa   em perfeitas condições de uso, equipamentos e ferramentas de uso permanente ou temporário, constante na relação que compõe este Termo, para uso exclusivo da minha função, independentemente do regime de trabalho ao qual esteja vinculado (permanente na sede da empresa e/ou residência ou híbrido).
+        <br>
+        <br><br><br><span>Comprometo-me a:</span>
+        <br>
+        <br>1-	Zelar pela integridade de todo o material recebido, conservando-os em perfeito estado de conservação, limpeza e em condições de uso, jamais transferindo sua posse, ainda que temporária, para terceiros;
+        <br>2-	Mantê-los em local seguro, quando não utilizados;
+        <br>3-	Utilizar os objetos abaixo descritos exclusivamente para empresa, estando ciente de que é expressamente proibida a utilização para fins particulares;
+        <br>4-	Não efetuar qualquer alteração, modificação, agregação de componentes, acessórios ou programas diversos daqueles fornecidos pela empresa;
+        <br>5-	Não utilizar internet, para outros fins que não corporativos;
+        <br>6-	Comunicar prontamente a chefia imediata, qualquer deficiência do equipamento e/ou ferramenta que comprometa a utilização, solicitando a substituição, se for o caso, mediante devolução;
+        <br>7-	Responsabilizar-se por qualquer sinistro causado, por imprudência, imperícia ou negligência, nestas estando implícita perda ou furto. Excluído o desgaste sofrido pelo uso normal (como riscos e desgastes eventuais);
+        <br>8-	Sujeitar-se às penas de advertência / suspensão pelo descumprimento das normas da presente e na reincidência estar-se-á sujeito a demissão por justa causa, nos termos do artigo 482, alínea “e” da CLT;
+        <br>9-	Autorizar, nos termos do artigo 462, §1º da CLT, o desconto (no salário ou rescisão) do dano causado, em valores atualizados a época do efetivo pagamento;
+        <br>10-	Restituí-lo de imediato e em perfeito estado de conservação e limpeza como recebeu, salvo desgastes naturais, quando da extinção do contrato de trabalho ou promoção a outro cargo que não necessite da utilização dos aparelhos abaixo descritos.
+        <br>11	A não devolução em cinco dias úteis constante no item dezesseis, acarretará ao desconto na importância do valor de R$ 5.000 (Cinco mil reais) referente ao notebook e o valor de R$ 48,90 (Quarenta e oito reais e noventa centavos) referente a mochila, o portador não será reembolsado após descrito acima.
+        <br>12-	Não revelar, fora do âmbito profissional, fato ou informação de qualquer natureza de que tenha conhecimento por força de minhas atribuições, salvo em decorrência de decisão competente na esfera legal ou judicial;
+        <br>13-	Manter a necessária cautela quando da exibição de dados em tela, impressora ou na gravação em meios eletrônicos, a fim de evitar que deles venham a tomar ciência pessoas não autorizadas;
+        <br>14-	Não revelar a ninguém minha senha de acesso à rede corporativa, aos computadores, Internet e/ou de minha caixa postal (e-mail) coorporativo e tomar o máximo de cuidado para que ela permaneça somente de meu conhecimento;
+        <br>15-	Responder, em todas as instâncias, pelas consequências das ações ou omissões de minha parte que possam pôr em risco ou comprometer a exclusividade de conhecimento de minha senha ou das transações a que tenha acesso.
+        <br><br><br><br><span>Declaro, ainda, estar plenamente esclarecido e consciente que:</span>
+        <br>Não é permitida a navegação aos sites pertencentes às categorias abaixo:
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Pornográfico e de caráter sexual;
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Compartilhamento de arquivos (ex.: peer to peer, Bit Torrent, Emule, etc.);
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Pornografia infantil (pedofilia);
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Apologia ao terrorismo;
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Apologia às drogas;
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Crackers;
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Violência e agressividade (racismo, preconceito, etc.);
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Violação de direito autoral (pirataria, etc.);
+        <br>&nbsp;&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;&nbsp;Conteúdo impróprio, ofensivo, ilegal, discriminatório e similares
+        </p>
+        <table border="1">
+            <tr>
+                <th>Objeto</th>
+                <th>Marca</th>
+                <th>Identificação</th>
+            </tr>
+            <tr>
+                <td>Notebook ${modelo}</td>
+                <td>${marca}</td>
+                <td>${nomeMaquina}</td>
+            </tr>
+            <tr>
+                <td>Carregador</td>
+                <td>${carregador}</td>
+                <td>${locadora}-${patrimonio}</td>
+            </tr>
+            <tr>
+                <td>Mochila</td>
+                <td>${mochila}</td>
+            </tr>
+        </table>
+        `
 });
--- ti-entrega.js
+++ ti-entrega.js
@@ -1,28 +1,72 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
-    document.getElementById("nome-colaborador").addEventListener("input", function(){
-        let nome = this.value.toLowerCase().replace(/ /g, ".");
+    // Verificando o DOM
 
-        document.getElementById("email-colaborador").value = nome + "@workongroup.com.br"
+    // Transoformando o nome do colaborador em seu email
+    document.getElementById("nome-colaborador").addEventListener("input", function(){
+        let nome = this.value.toLowerCase().replace(/ /g, "."); // transformando a informação inteira em minúsucla e susbtituindo espaço po ponto
+
+        document.getElementById("email-colaborador").value = nome + "@workongroup.com.br" // atribuindo informação (dado) ao input
     });
     
 
-    let formulario = document.getElementById("formulario");
-    formulario.addEventListener("submit", function(event){
-        event.preventDefault();
-        let bruto_nome = document.getElementById("nome-colaborador").value.trim()
-        let nome_sobrenome = bruto_nome.split(" ")
-        let nome = nome_sobrenome[0]
-        let sobrenome = nome_sobrenome[1]
-        let email = nome+"_"+sobrenome+"@workongroup.com.br"
 
-        let resultado = document.getElementById("resultado")
-        resultado.innerHTML = `
-            Nome Inteiro: ${nome_sobrenome}
-            <br>Nome ${nome}
-            <br>Sobrenome: ${sobrenome}
-            <br>email: ${email}
-          `;
+    let formulario = document.getElementById("formulario"); // armazenando o fomulario em uma variável
+    formulario.addEventListener("submit", function(event){
+        event.preventDefault(); // nao deixa o a página resetar e perder as infos
+
+        // coletando as informações do usuário
+        let nome = document.getElementById("nome-colaborador").value // salvando os dados dentro de uma variável
+        localStorage.setItem("nomeColaborador", nome); // pegando os dados dessa variavel e salvando no localStorage
+
+        let email = document.getElementById("email-colaborador").value
+        localStorage.setItem("emailColaborador", email);
+
+        let rG = document.getElementById("rg").value
+        localStorage.setItem("rg", rG);
+
+        let cpf = document.getElementById("cpf").value
+        localStorage.setItem("cpf", cpf);
+
+        let funcao = document.getElementById("funcao").value
+        localStorage.setItem("funcao", funcao);
+
+        let setor = document.getElementById("setor").value
+        localStorage.setItem("setor", setor);
+
+        let escolhaEmpresa = document.getElementById("escolha-empresa").value
+        localStorage.setItem("escolhaEmpresa", escolhaEmpresa);
+
+
+
+
+
+        // coletando as informações da máquina
+
+        let nomeMaquina = document.getElementById("nome-maquina").value
+        localStorage.setItem("nomeMaquina", nomeMaquina);
+        
+        let patrimonio = document.getElementById("patrimonio").value
+        localStorage.setItem("patrimonio", patrimonio);
+        
+        let locadora = document.getElementById("locadora").value
+        localStorage.setItem("locadora", locadora);
+        
+        let marca = document.getElementById("marca").value
+        localStorage.setItem("marca", marca);
+
+        let modelo = document.getElementById("modelo").value
+        localStorage.setItem("modelo", modelo);
+
+        let carregador = document.getElementById("carregador");
+        localStorage.setItem("carregador", carregador.checked ? "Sim" : "Não");
+
+        let mochila = document.getElementById("mochila");
+        localStorage.setItem("mochila", mochila.checked ? "Sim" : "Não");
+
+
+        setTimeout(() => {
+            window.location.href = "/setores/ti/entrega-imprimir.html";
+        }, 100);
     });
 
     let p_button = document.getElementsByClassName("p")
